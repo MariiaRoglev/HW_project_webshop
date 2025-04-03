b0f38ca76629de8658bc67b7a68c7d2ad80b4175
MariiaRoglev <Mariarogaleva06@gmail.com>
2025-04-03 15:19:31 +0300

    HW_03.04

--- src/main/java/com/project/fw/ApplicationManager.java
+++ src/main/java/com/project/fw/ApplicationManager.java
@@ -2,32 +2,40 @@ package com.project.fw;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.time.Duration;
 
-public class ApplicationManager
-{
+public class ApplicationManager {
 
+    String browser;
     WebDriver driver;
 
     UserHelper user;
     ProductHelper product;
 
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
 
     public void init() {
+//for cross-Browser tests
+
+        if (browser.equalsIgnoreCase("chrome")) {
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            driver = new FirefoxDriver();
+        }
 
 
-        driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
         driver.get("https://demowebshop.tricentis.com/");
 
-        user=new UserHelper(driver);
-        product=new ProductHelper(driver);
+        user = new UserHelper(driver);
+        product = new ProductHelper(driver);
     }
-
-
 
 
     public void stop() {
@@ -41,6 +49,7 @@ public class ApplicationManager
     public ProductHelper getProduct() {
         return product;
     }
-
-
 }
+
+
+
--- src/test/java/com/webshop/tests/TestBase.java
+++ src/test/java/com/webshop/tests/TestBase.java
@@ -1,6 +1,7 @@
 package com.webshop.tests;
 
 import com.project.fw.ApplicationManager;
+import org.openqa.selenium.remote.Browser;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
@@ -8,7 +9,7 @@ import org.testng.annotations.BeforeSuite;
 
 public class TestBase
 {
-    protected static ApplicationManager app = new ApplicationManager();//add static
+    protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", Browser.CHROME.browserName()));//add static. in() for testing in chrome
 
     //@BeforeMethod
     @BeforeSuite
