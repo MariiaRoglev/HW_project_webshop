4d09c32bae33e261129b441e59a69187d732a20d
MariiaRoglev <Mariarogaleva06@gmail.com>
2025-04-06 19:40:20 +0300

    HW_06.04

--- src/test/java/com/webshop/tests/TestBase.java
+++ src/test/java/com/webshop/tests/TestBase.java
@@ -6,24 +6,42 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import java.lang.reflect.Method;
 
 public class TestBase
 {
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
+
     protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", Browser.CHROME.browserName()));//add static. in() for testing in chrome
 
-    //@BeforeMethod
     @BeforeSuite
     public void setUp()
     {
         app.init();
     }
 
-    //@AfterMethod(enabled = false)
     @AfterSuite (enabled = false)
     public void tearDown()
     {
         app.stop();
     }
+
+
+    @BeforeMethod
+    public void startTest(Method method)
+    {
+        logger.info("Start test" +" " +method.getName());
+    }
+
+    @AfterMethod
+    public void stopTest()
+    {
+        logger.info("Stop test");
+    }
+
+
 
 
 }
